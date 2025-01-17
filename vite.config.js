849e692cb946daf90170516ff6283e53a0210580
Elchin <elchinnovruzovv@gmail.com>
2025-01-17 22:06:15 +0100

    copy version

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: './', // Ensures relative paths for assets
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                analyze: './analyzeWallet.html',
+                ranking: './ranking.html',
+                achievements: './achievements.html',
+            },
+        },
+    },
     optimizeDeps: {
         include: ['theblockchainapi'], // Add the package to optimize CommonJS support
     },
